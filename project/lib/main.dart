27451b4ff1a6e49ff80fbb327ac876d40bc21943
Roope1 <roope70@live.com>
2022-04-20 13:30:10 +0300

    got rid of google font import
    downloaded the actual font to the project

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -2,35 +2,28 @@
 /// Title: Sanity in the times of COVID-19
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
+var titleTextStyle = const TextStyle(fontSize: 72, color: Colors.black);
+var buttonTextStyle = const TextStyle(fontSize: 30, color: Colors.black);
 
-var titleTextStyle = GoogleFonts.robotoMono(
-  fontSize: 72,
-  color: Colors.black );
+const double textBoxWidth = 1000;
 
-var buttonTextStyle = GoogleFonts.robotoMono(
-  fontSize: 30,
-  color: Colors.black);
-  
-const double textBoxWidth = 800;
+var textBoxDec = const BoxDecoration(
+    color: Color(0xffDFF3FF),
+    borderRadius: BorderRadius.all(
+      Radius.circular(12.0),
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black87,
+        blurRadius: 10.0,
+      )
+    ]);
 
-var textBoxDec = const BoxDecoration (
-  color: Color(0xffDFF3FF),
-  borderRadius: BorderRadius.all( 
-    Radius.circular(12.0),
-  ),
-);
+var textBoxPadding = const EdgeInsets.symmetric(vertical: 10, horizontal: 20);
+var textBoxMargin = const EdgeInsets.symmetric(horizontal: 10, vertical: 50);
 
-var textBoxPadding = const EdgeInsets.symmetric(
-  vertical: 10, 
-  horizontal: 20,
-);
-
-var textBoxMargin = const EdgeInsets.symmetric(
-  horizontal: 10,
-  vertical: 20
-);
+var contentStyle = const TextStyle(fontFamily: "RobotoMono", fontSize: 16);
 
 void main() {
   runApp(const MyApp());
@@ -53,60 +46,53 @@ class CovidWebsite extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return Scaffold(
       body: SingleChildScrollView(
-        child: Column ( 
-          children: <Widget>[ 
-          
-            //Title box
-            Container(  
-              decoration: const BoxDecoration(
+          child: Column(
+        children: <Widget>[
+          //Title box
+          Container(
+            decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage("assets/Images/BGImage.jpg"),
-                  fit: BoxFit.fitHeight,
-                  )
-                
-               ),
-              height: MediaQuery.of(context).size.height / 1.5,
-              width: MediaQuery.of(context).size.width,
-              padding: const EdgeInsets.only(left: 100),
-              child: Align (
-                alignment: Alignment.centerLeft,
-                child:Text(
-                  "Sanity in the times\nof COVID-19",
-                  style: titleTextStyle,
-                ),
+              image: AssetImage("assets/Images/BGImage.jpg"),
+              fit: BoxFit.fitHeight,
+            )),
+            height: MediaQuery.of(context).size.height / 1.5,
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.only(left: 100),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Sanity in the times\nof COVID-19",
+                style: titleTextStyle,
               ),
             ),
+          ),
 
-            // Navigation bar
-            const NavBar(),
+          // Navigation bar
+          const NavBar(),
 
-            // Actual page content
-            Container(
+          // Actual page content
+          Container(
               width: double.infinity,
-              decoration: const BoxDecoration( 
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
                     Colors.blue,
                     Colors.white,
-                  ]
-                )
-              ),
-              child: Column ( 
+                  ])),
+              child: Column(
                 children: const <Widget>[
                   SocialContent(),
                   PhysicalContent(),
                   MentalContent(),
                   ResourcesContent(),
                 ],
-              )
-            ) 
-          ],
-        )  
-      ),
+              ))
+        ],
+      )),
     );
   }
 }
@@ -117,21 +103,21 @@ class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-     color: Colors.teal,
-     height: 70,
-     width: MediaQuery.of(context).size.width,
-     child: Row(
-       mainAxisAlignment: MainAxisAlignment.center,
-       crossAxisAlignment: CrossAxisAlignment.center,
+      color: Colors.teal,
+      height: 70,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
 
-     // Navigation bar buttons
-       children:  const <Widget>[
-         NavButton("Social"),
-         NavButton("Mental"),
-         NavButton("Physical"),
-         NavButton("Resources"),
-       ],
-     ),
+        // Navigation bar buttons
+        children: const <Widget>[
+          NavButton("Social"),
+          NavButton("Mental"),
+          NavButton("Physical"),
+          NavButton("Resources"),
+        ],
+      ),
     );
   }
 }
@@ -143,7 +129,7 @@ class NavButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container( 
+    return Container(
       padding: const EdgeInsets.symmetric(horizontal: 100),
       child: Text(title, style: buttonTextStyle),
     );
@@ -155,21 +141,21 @@ class SocialContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: textBoxDec,
-      padding: textBoxPadding,
-      margin: textBoxMargin,
-      alignment: Alignment.center,  
-      width: textBoxWidth, 
-      child : Column(children: [
-        Text("Social", style: buttonTextStyle),
-        //Actual text
-        Text(
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          style: GoogleFonts.robotoMono(fontSize: 16),
-        )
-       ],
-      )
-    );
+        decoration: textBoxDec,
+        padding: textBoxPadding,
+        margin: textBoxMargin,
+        alignment: Alignment.center,
+        width: textBoxWidth,
+        child: Column(
+          children: [
+            Text("Social", style: buttonTextStyle),
+            //Actual text
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              style: contentStyle,
+            )
+          ],
+        ));
   }
 }
 
@@ -178,21 +164,21 @@ class PhysicalContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: textBoxDec,
-      margin: textBoxMargin,
-      padding: textBoxPadding,
-      alignment: Alignment.center,
-      width: textBoxWidth, 
-      child : Column(children: [
-        Text("Physical", style: buttonTextStyle),
-        //Actual text
-        Text(
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          style: GoogleFonts.robotoMono(fontSize: 16),
-        )
-       ],
-      )
-    );
+        decoration: textBoxDec,
+        margin: textBoxMargin,
+        padding: textBoxPadding,
+        alignment: Alignment.center,
+        width: textBoxWidth,
+        child: Column(
+          children: [
+            Text("Physical", style: buttonTextStyle),
+            //Actual text
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              style: contentStyle,
+            )
+          ],
+        ));
   }
 }
 
@@ -201,21 +187,21 @@ class MentalContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: textBoxDec,
-      padding: textBoxPadding,
-      margin: textBoxMargin,
-      alignment: Alignment.center, 
-      width: textBoxWidth, 
-      child : Column(children: [
-        Text("Mental", style: buttonTextStyle),
-        //Actual text
-        Text(
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          style: GoogleFonts.robotoMono(fontSize: 16),
-        )
-       ],
-      )
-    );
+        decoration: textBoxDec,
+        padding: textBoxPadding,
+        margin: textBoxMargin,
+        alignment: Alignment.center,
+        width: textBoxWidth,
+        child: Column(
+          children: [
+            Text("Mental", style: buttonTextStyle),
+            //Actual text
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              style: contentStyle,
+            )
+          ],
+        ));
   }
 }
 
@@ -224,20 +210,20 @@ class ResourcesContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: textBoxDec,
-      padding: textBoxPadding,
-      margin: textBoxMargin,
-      alignment: Alignment.center,
-      width: textBoxWidth, 
-      child : Column(children: [
-        Text("Resources", style: buttonTextStyle),
-        //Actual text
-        Text(
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          style: GoogleFonts.robotoMono(fontSize: 16),
-        )
-       ],
-      )
-    );
+        decoration: textBoxDec,
+        padding: textBoxPadding,
+        margin: textBoxMargin,
+        alignment: Alignment.center,
+        width: textBoxWidth,
+        child: Column(
+          children: [
+            Text("Resources", style: buttonTextStyle),
+            //Actual text
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              style: contentStyle,
+            )
+          ],
+        ));
   }
 }
